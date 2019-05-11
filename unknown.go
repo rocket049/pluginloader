ecf7984021d795c8e4837a9907683ebc518f9e2c
rocket049 <fuhuizn@163.com>
2019-05-11 15:44:52 +0800

    modify CopyToStruct ,not depend gob2019年 05月 11日 星期六 15:44:52 CST

--- unknown.go
+++ unknown.go
@@ -1,9 +1,8 @@
 package pluginloader
 
 import (
-	"bytes"
-	"encoding/gob"
 	"encoding/json"
+	"errors"
 	"reflect"
 )
 
@@ -56,16 +55,55 @@ func (s *UnknownObject) Json() []byte {
 	return res
 }
 
-//CopyToStruct 利用 gob 编码技术把结构体的值复制到 v 中，v 必须是相似结构体的指针。 copy the value of a struct to v through gob encoding.
+//CopyToStruct 利用 reflect 技术把结构体的可 export 值复制到 v 中，v 必须是相似结构体的指针。 copy the exported value of a struct to v through gob encoding.
 func (s *UnknownObject) CopyToStruct(v interface{}) error {
-	buf := bytes.NewBufferString("")
-	encoder := gob.NewEncoder(buf)
-	err := encoder.Encode(s.V.Interface())
+	return structCopy(s.V.Interface(), v)
+}
+
+func structCopy(src, dst interface{}) error {
+	srcV, err := srcFilter(src)
 	if err != nil {
 		return err
 	}
-	reader := bytes.NewReader(buf.Bytes())
-	decoder := gob.NewDecoder(reader)
-	err = decoder.Decode(v)
-	return err
+	dstV, err := dstFilter(dst)
+	if err != nil {
+		return err
+	}
+	srcKeys := make(map[string]bool)
+	for i := 0; i < srcV.NumField(); i++ {
+		srcKeys[srcV.Type().Field(i).Name] = true
+	}
+	for i := 0; i < dstV.Elem().NumField(); i++ {
+		fName := dstV.Elem().Type().Field(i).Name
+		if _, ok := srcKeys[fName]; ok {
+			v := srcV.FieldByName(dstV.Elem().Type().Field(i).Name)
+			if v.CanInterface() {
+				dstV.Elem().Field(i).Set(v)
+			}
+		}
+	}
+
+	return nil
+}
+
+func srcFilter(src interface{}) (reflect.Value, error) {
+	v := reflect.ValueOf(src)
+	if v.Type().Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		return reflect.Zero(v.Type()), errors.New("src type error: not a struct or a pointer to struct")
+	}
+	return v, nil
+}
+
+func dstFilter(src interface{}) (reflect.Value, error) {
+	v := reflect.ValueOf(src)
+	if v.Type().Kind() != reflect.Ptr {
+		return reflect.Zero(v.Type()), errors.New("src type error: not a pointer to struct")
+	}
+	if v.Elem().Kind() != reflect.Struct {
+		return reflect.Zero(v.Type()), errors.New("src type error: not point to struct")
+	}
+	return v, nil
 }
